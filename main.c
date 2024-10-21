9ea976b78734669148bb2f854b03fa438d4c6cc8
Benjamin Barlet <benjamin.barlet@free.fr>
2024-10-21 16:58:12 +0200

    combinaison style + affichage

--- main.c
+++ main.c
@@ -1,37 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 
 
-char* init (int Height, int Width);
-char* bateau (char*,int, int);
-char* affiche (char*, int, int);
+struct Case {
+    char value;
+    char couleur;
+    char background;
+};
+
+
+
+struct Case* init (int Height, int Width);
+struct Case* bateau (struct Case*,int, int);
+struct Case* affiche (struct Case*, int, int);
+void ecrire(struct Case);
 
 
 
 
 int main(void) {
+
+
+    #ifdef _WIN32
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+    #endif
+
+
     const int Height = 9;
     const int Width = 9;
-    char* tableau = init(Height, Width);
+    struct Case* tableau = init(Height, Width);
     bateau(tableau,Height, Width);
 
     free(tableau);
     return 0;
 }
 
-char* init (int Height, int Width){
+struct Case* init (int Height, int Width){
 
 
 
-    char* tableau =(char*)malloc(sizeof(char)*(Height*Width));
+    struct Case* tableau =(struct Case*)malloc(sizeof(struct Case)*(Height*Width));
     if (tableau == NULL) {
         printf("PROBLEME\n");
         exit(EXIT_FAILURE);
     }
 
     for (int i = 0; i < Height * Width; i++){
-        tableau [i] = 'A';
+        tableau [i].value = 'A';
+        tableau [i].background = 'r';
+        tableau [i].couleur = 'B';
     }
 
 
@@ -41,25 +66,56 @@ char* init (int Height, int Width){
 
 }
 
-char* bateau(char* tableau, int Height, int Width){
+struct Case* bateau(struct Case* tableau, int Height, int Width){
     int xBateau;
     int yBateau;
 
     scanf("%d", &xBateau);
     scanf("%d", &yBateau );
-    tableau[(yBateau - 1) * Width + xBateau - 1] = 'B';
+    tableau[(yBateau - 1) * Width + xBateau - 1].background = 'b';
 
     affiche(tableau, Height, Width);
     return tableau;
 
 }
 
-char* affiche (char* tableau, int Height, int Width){
+struct Case* affiche (struct Case* tableau, int Height, int Width){
     for (int i = 0; i < Height; i++){
         for (int j = 0; j < Width; j++){
-            printf("%c", tableau[i * Width + j]);
+            ecrire(tableau[i * Width + j]);
 
         }
         printf("\n");
 
     }}
+
+
+
+
+
+void ecrire(struct Case c) {
+    switch (c.couleur) {
+        case 'r':printf("\033[0;31m");break;
+        case 'g':printf("\033[0;32m");break;
+        case 'y':printf("\033[0;33m");break;
+        case 'b':printf("\033[0;34m");break;
+        case 'm':printf("\033[0;35m");break;
+        case 'c':printf("\033[0;36m");break;
+        case 'w':printf("\033[0;37m");break;
+        case 'B':printf("\033[0;30m");break;
+        default:printf("\033[0;30m");break;
+    }
+    switch (c.background) {
+        case 'r':printf("\033[0;41m");break;
+        case 'g':printf("\033[0;42m");break;
+        case 'y':printf("\033[0;43m");break;
+        case 'b':printf("\033[0;44m");break;
+        case 'm':printf("\033[0;45m");break;
+        case 'c':printf("\033[0;46m");break;
+        case 'w':printf("\033[0;47m");break;
+        case 'B':printf("\033[0;40m");break;
+        default:printf("\033[0;40m");break;
+    }
+    printf("%c", c.value);
+    printf("\033[0m");
+}
