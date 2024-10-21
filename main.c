9d6f21fd1be552ac1919003b1b414be6cea2ec5d
Benjamin Barlet <benjamin.barlet@free.fr>
2024-10-21 15:34:50 +0200

    Maxi banger grille malloc

--- main.c
+++ main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int Height;
+int Width;
+
+void init (void){
+    printf("%s", "donnez la hauteur \n");
+    scanf("%d", &Height);
+    printf("%s", "donnez la largeur \n");
+    scanf("%d", &Width);
+    char* tableau =(char*)malloc(sizeof(char)*(Height*Width));
+    if (tableau == NULL) {
+        printf("PROBLEME\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < Height * Width; i++){
+        tableau [i] = 'A';
+    }
+
+
+    for (int i = 0; i < Height; i++){
+        for (int j = 0; j < Width; j++){
+            printf("%c", tableau[i * Width + j]);
+
+        }
+        printf("\n");
+    }
+
+
+
+    free(tableau);
+
+
+}
+
 
 int main(void) {
-    printf("Hello, World babz!\n");
+
+
+
+    init();
+    printf("Hauteur: %d, Largeur: %d\n", Height, Width);
+
+
     return 0;
 }
