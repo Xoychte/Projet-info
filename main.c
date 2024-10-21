19d6a527f66a893106ca4493fb9720e67b644be2
Benjamin Barlet <benjamin.barlet@free.fr>
2024-10-21 16:16:33 +0200

    Fonction affiche + bateau

--- main.c
+++ main.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-void init (void);
+
+
+
+char* init (int Height, int Width);
+char* bateau (char*,int, int);
+char* affiche (char*, int, int);
 
 
 
 
 int main(void) {
+    const int Height = 9;
+    const int Width = 9;
+    char* tableau = init(Height, Width);
+    bateau(tableau,Height, Width);
 
-    init();
-
+    free(tableau);
     return 0;
 }
 
-void init (void){
-    int Height;
-    int Width;
+char* init (int Height, int Width){
 
-    printf("%s", "donnez la hauteur \n");
-    scanf("%d", &Height);
-    printf("%s", "donnez la largeur \n");
-    scanf("%d", &Width);
+
 
     char* tableau =(char*)malloc(sizeof(char)*(Height*Width));
     if (tableau == NULL) {
@@ -32,17 +35,31 @@ void init (void){
     }
 
 
+    affiche(tableau, Height, Width);
+
+    return tableau;
+
+}
+
+char* bateau(char* tableau, int Height, int Width){
+    int xBateau;
+    int yBateau;
+
+    scanf("%d", &xBateau);
+    scanf("%d", &yBateau );
+    tableau[(yBateau - 1) * Width + xBateau - 1] = 'B';
+
+    affiche(tableau, Height, Width);
+    return tableau;
+
+}
+
+char* affiche (char* tableau, int Height, int Width){
     for (int i = 0; i < Height; i++){
         for (int j = 0; j < Width; j++){
             printf("%c", tableau[i * Width + j]);
 
         }
         printf("\n");
-    }
 
-
-
-    free(tableau);
-
-
-}
+    }}
